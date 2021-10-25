76750109045d23401df366931550cd67b53866a7
LAPTOP-1RQ0IJS9\beqak <kikutadze.beka@kiu.edu.ge>
2021-10-25 23:45:01 +0400

    hgvjh

--- src/pgdp/luckypenguin/LuckyPenguin.java
+++ src/pgdp/luckypenguin/LuckyPenguin.java
@@ -18,7 +18,7 @@ public class LuckyPenguin {
             pingu[i] = per;
         }
         boolean t = false;
-        int fish[] = new int[9];
+        int fish[] = new int[10];
         int checkpingu[] = new int[num];
         int k = 0;
         int fir, sec;
@@ -28,7 +28,7 @@ public class LuckyPenguin {
                 continue;
             }
             int sum = 0;
-            writeBoard(fish[0],fish[1], fish[2], fish[3], fish[4], fish[5], fish[6], fish[7], fish[8]);
+            writeBoard(fish[1], fish[2], fish[3], fish[4], fish[5], fish[6], fish[7], fish[8], fish[9]);
             System.out.println("It's penguin " + k + " turn:");
             int stop = 0;
             for (int i = 0; i < num; i++) {
@@ -36,7 +36,7 @@ public class LuckyPenguin {
             }
             if (stop == num - 1) {
                 System.out.println("You are the last penguin to play! You win all the fish on the board!");
-                for (int i = 0; i < 9; i++) {
+                for (int i = 1; i < 10; i++) {
                     pingu[k] += fish[i];
                 }
                 t = true;
@@ -49,30 +49,30 @@ public class LuckyPenguin {
             if (sum == 12) {
 
                 System.out.println("King Penguin! You win all the fish on the board!");
-                for (int i = 0; i < 9; i++) {
+                for (int i = 1; i < 10; i++) {
                     pingu[k] += fish[i];
                     fish[i] = 0;
                 }
 
             } else if (sum == 2) {
                 System.out.println("Lucky penguin! You win all fish on the board except F7!");
-                for (int i = 0; i < 9; i++) {
-                    if (i == 4) continue;
+                for (int i = 1; i < 10; i++) {
+                    if (i == 5) continue;
                     pingu[k] += fish[i];
                     fish[i] = 0;
                 }
             } else if (sum == 7) {
                 System.out.println("Wedding! You give a fish and place it on F7.");
-                fish[4]++;
+                fish[5]++;
                 pingu[k]--;
-            } else if (fish[sum - 3] == 0) {
+            } else if (fish[sum - 2] == 0) {
                 System.out.println("You put a fish on F" + sum + ".");
                 pingu[k]--;
-                fish[sum - 3]++;
+                fish[sum - 2]++;
             } else {
                 System.out.println("They take the fish from F" + sum + ".");
                 pingu[k]++;
-                fish[sum - 3]--;
+                fish[sum - 2]--;
             }
             if (pingu[k] == 1) System.out.println("You now have " + pingu[k] + " fish!");
             else System.out.println("You now have " + pingu[k] + " fishes!");
